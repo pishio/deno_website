39339e24ec695896b72e02b6e4b2859e3c3468e7
pishio <mail@pishio.com>
2021-04-02 11:11:03 +0900

    first

---
+++ index.tsx
@@ -0,0 +1,27 @@
+import { createRouter } from 'https://denopkg.com/keroxp/servest/router.ts';
+import React from 'https://dev.jspm.io/react';
+import ReactDOMServer from 'https://dev.jspm.io/react-dom/server';
+
+import App from './App.tsx';
+
+const router = createRouter();
+router.handle('/', async req => {
+  await req.respond({
+    headers: new Headers({
+      'content-type': 'text/html; charset=UTF-8'
+    }),
+    body: ReactDOMServer.renderToString(
+      <html>
+        <head>
+          <title>deno react ssr</title>
+        </head>
+        <body>
+          <App />
+        </body>
+      </html>
+    ),
+    status: 200
+  })
+});
+
+router.listen('0.0.0.0:8000');
