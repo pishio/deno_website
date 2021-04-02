190e69eb75ae5a1546ec6d765286d2e20663f8d8
pishio <mail@pishio.com>
2021-04-02 11:32:21 +0900

    何requestしてもHTMLを返す

--- index.tsx
+++ index.tsx
@@ -1,6 +1,32 @@
-addEventListener("fetch", (event) => {
-  const response = new Response("Hello World!", {
-    headers: { "content-type": "text/plain" },
+import React from 'https://dev.jspm.io/react';
+import ReactDOMServer from 'https://dev.jspm.io/react-dom/server';
+
+import App from './App.tsx';
+
+
+
+function handleRequest(request) {
+
+  const html = ReactDOMServer.renderToString(
+    <html>
+      <head>
+        <title>deno react ssr</title>
+      </head>
+      <body>
+        <App />
+      </body>
+    </html>
+  )
+
+  return new Response(
+    html, {
+    headers: {
+      "content-type": "text/html; charset=UTF-8",
+    },
   });
-  event.respondWith(response);
+  
+}
+
+addEventListener("fetch", (event) => {
+  event.respondWith(handleRequest(event.request));
 });
