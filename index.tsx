4c7b4961b96dedf646f1fb829d2328a7506e35ef
pishio <mail@pishio.com>
2021-04-02 11:30:38 +0900

    hello worldをコピペ

--- index.tsx
+++ index.tsx
@@ -1,37 +1,6 @@
-function handleRequest(request) {
-  const { pathname } = new URL(request.url);
-
-  // Respond with HTML
-  if (pathname.startsWith("/html")) {
-    const html = `<html>
-      <p><b>Message:</b> Hello from Deno Deploy.</p>
-      </html>`;
-
-    return new Response(html, {
-      headers: {
-        // The "text/html" part implies to the client that the content is HTML
-        // and the "charset=UTF-8" part implies to the client that the content
-        // is encoded using UTF-8.
-        "content-type": "text/html; charset=UTF-8",
-      },
-    });
-  }
-
-  // Respond with JSON
-  if (pathname.startsWith("/json")) {
-    // Use stringify function to convert javascript object to JSON string.
-    const json = JSON.stringify({
-      message: "Hello from Deno Deploy",
-    });
-
-    return new Response(json, {
-      headers: {
-        "content-type": "application/json; charset=UTF-8",
-      },
-    });
-  }
-}
-
 addEventListener("fetch", (event) => {
-  event.respondWith(handleRequest(event.request));
+  const response = new Response("Hello World!", {
+    headers: { "content-type": "text/plain" },
+  });
+  event.respondWith(response);
 });
