a8286340f6ad38dc5b87a77ad1cb690384de1afa
pishio <mail@pishio.com>
2022-02-21 16:22:06 +0900

    test

--- App.tsx
+++ App.tsx
@@ -1,3 +1,9 @@
-const App = () => (<div>Hello Deno React</div>)
+/** @jsx h */
+/// <reference no-default-lib="true"/>
+/// <reference lib="dom" />
+/// <reference lib="dom.asynciterable" />
+/// <reference lib="deno.ns" />
+import React from "https://jspm.dev/react@17.0.2";
+const App = () => <div>Hello Deno React</div>;
 
-export default App
+export default App;
